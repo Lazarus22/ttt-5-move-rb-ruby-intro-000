4d699f9b0bbe696d7277c65c7bee6f8324c4b00c
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-06-20 17:09:24 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,16 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def imput_to_index(move)
+  move_adjust = "#{move} - 1"
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+def update_array_at_with(board, move_adjust, "X")
+  array[board] = move_adjust
+end
+
+
+
 # code your input_to_index and move method here!
